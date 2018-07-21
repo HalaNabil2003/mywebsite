c204014537fb2023543f8ae5f9fde1936d188efa
HalaNabil2003 <hla.n.free@gmail.com>
2018-07-21 13:07:13 +0300

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
 
-@app.route("/")
-def website():
-	return "Hello, World!"
+# app=Flask(__name__)
+
+# import random
+# from random import randit
 
 
-if __name__== "__main__":
-	app.run()
+
+# @app.route("/hello")
+# def hello():
+# 	return "Hello World!"
+
+# @app.route("/Goodbye")
+# def Goodbye():
+# 	return "Goodbye!"
+# @app.route("/")
+# def index():
+# 	return "My name is em ellol"
+# if __name__=="__main__":
+# 	app.run(port=4000, debug=True)
+
+	
+# 	
+from flask import Flask, render_template
+# import random
+app=Flask(__name__)
+@app.route("/web")
+# def quote():
+# 	h=["my name is Hala","I am 15","my fav color is blue"]
+# 	x=(random.choice(h))
+# 	return x
+def num():
+	return render_template("mywebsite.html")
+
+if __name__=="__main__":
+	app.run(port=7000, debug=True)
